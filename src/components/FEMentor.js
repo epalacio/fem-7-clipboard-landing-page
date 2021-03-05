46a6ba4f95dd5bf14f532b58a6897562f7d525cb
Esteban Palacio <estipalacio@gmail.com>
2021-03-05 10:32:52 -0500

    added FEM footer

---
+++ src/components/FEMentor.js
@@ -0,0 +1,11 @@
+const FEMentor = () => {
+    return (
+        <div>
+            <p class="attribution">Challenge by <a rel="noreferrer" href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+      Coded by <a href="https://www.estebanpalacio.com">Esteban Palacio</a>.
+            </p>
+        </div>
+    );
+}
+ 
+export default FEMentor;
